d4338cbd54e2e5bc3a8730a13584908c97029ac2
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-03 17:29:19 +0100

    Add: project class with it's methods

---
+++ src/functionality.js
@@ -0,0 +1,40 @@
+// Array to store all projects.
+const projectLibrary = [];
+const currentActiveProject = null; // Variable which will hold current active project if none it is set to null.
+
+// Project creating class.
+export default class Project {
+    static id = 0;
+    constructor(name) {
+        this.id = `Project-${++Project.id}`;
+        this.name = name;
+        this.tasks = [];
+    }
+
+    createTask(taskName, dueDate, importance, description) {
+        const task = {
+            id: `task-${Math.floor(Math.random() * Date.now()).toString(36)}`,
+            priority: importance,
+            name: taskName,
+            due: dueDate,
+            desc: description,
+            isCompleted: false
+        }
+        this.tasks.push(task);
+        return task;
+    }
+
+    removeTask(taskId) {
+        const idx = this.tasks.indexOf(task => task.id === taskId);
+        if (idx !== -1) {
+            this.tasks.splice(idx, 1);
+        }
+    }
+
+    toggleTaskStatus(taskId) {
+        const task = this.tasks.find(task => task.id === taskId);
+        if (task) {
+            task.isCompleted = !task.isCompleted;
+        }
+    }
+}
